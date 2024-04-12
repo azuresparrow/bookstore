c75f164c68da9b1d3ac9945df644f87d21128740
James <jrogers2fc@gmail.com>
2024-04-12 10:02:16 -0700

    fix tests

--- tests/books.test.js
+++ tests/books.test.js
@@ -9,25 +9,32 @@ const db = require("../db");
 
 let book_isbn;
 
+
+
 beforeEach(async() => { 
-    let result = await db.query(`INSERT INTO books (isbn, amazon_url, author, language, pages, publisher, title, year) VALUES '0691161518', 'http://a.co/eobPtX2', 'Matthew Lane', 'english', 264, 'Princeton University Press', 'Power-Up: Unlocking the Hidden Mathematics in Video Games', 2017) RETURNING isbn`);
-    book_isbn = result.row[0].isbn
+    let result = await db.query(`INSERT INTO books (isbn, amazon_url, author, language, pages, publisher, title, year) VALUES ('0691161518', 'http://a.co/eobPtX2', 'Matthew Lane', 'english', 264, 'Princeton University Press', 'Power-Up: Unlocking the Hidden Mathematics in Video Games', 2017) RETURNING isbn`);
+    book_isbn = result.rows[0].isbn
 });
+
+
 
 // TEST POST
 describe("Create a new book", function() {
     test("POSTS /books", async function (){
-        const response = (await request(app).post(`/books`)).send({isbn: "0691161518",
-        amazon_url: "http://a.co/eobPtX2",
+        const response = (await request(app).post(`/books`).send({isbn: "0691161519",
+        amazon_url: "http://a.co/eobPtX3",
         author: "Matthew Lane",
         language: "english",
         pages: 264,
         publisher: "Princeton University Press",
-        title: "Power-Up: Unlocking Hidden Math in Video Games",
-        year: 2017})
+        title: "Power-Up: Unlocking Hidden Math in Video Games 2",
+        year: 2019}));
+        console.log(response);
+        expect(response.statusCode).toBe(201);
+        expect(response.body.book).toHaveProperty('isbn');
+
     });
-    expect(response.statusCode).toBe(201);
-    expect(response.body.book).toHaveProperty('isbn');
+    
 });
 
 //Tests the single book get
@@ -42,7 +49,7 @@ describe("Gets the book by isbn", function(){
 describe("Gets the book list", function(){
     test("Retrieves only book posted", async function (){
         const response = await request(app).get(`/books`);
-        expect(response.books).toHaveLength(1);
+        expect(response.body.books).toHaveLength(1);
         expect(response.body.books[0]).toHaveProperty('isbn');
         expect(response.body.books[0].isbn).toBe(book_isbn);
     })
